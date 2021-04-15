ed33a9abd2b06e34293b1a80569c58b8fea238b9
Jano Paetzold <jep@webfactory.de>
2021-04-15 17:13:03 +0200

    Document FilterStrategy interface (Case 125045)

--- Filter/Strategy/FilterStrategy.php
+++ Filter/Strategy/FilterStrategy.php
@@ -2,7 +2,14 @@
 
 namespace Webfactory\VisibilityFilterBundle\Filter\Strategy;
 
+/**
+ * Implement this to apply custom filtering logic to your project.
+ */
 interface FilterStrategy
 {
+    /**
+     * @param string $visibilityFieldAlias SQL alias for the visibility field of the requested entity
+     * @return string A string of SQL that will be appended as a WHERE clause to any query requesting an entity with a visibility field
+     */
     public function getFilterSql(string $visibilityFieldAlias): string;
 }

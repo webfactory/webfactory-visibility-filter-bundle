cc854e81300ba73c15fede757a328eb639b5e1fe
Jano Paetzold <jep@webfactory.de>
2021-04-16 16:05:38 +0200

    Make ValueInField filter strategy support ints and bools (Case 125045)

--- src/Filter/Strategy/ValueInField.php
+++ src/Filter/Strategy/ValueInField.php
@@ -11,17 +11,36 @@ use Webfactory\VisibilityFilterBundle\Filter\Strategy\FilterStrategy;
 final class ValueInField implements FilterStrategy
 {
     /**
-     * @var string
+     * @var string|int|bool
      */
     private $visibleValue;
 
-    public function __construct(string $visibleValue)
+    /**
+     * @param string|int|bool $visibleValue
+     */
+    public function __construct($visibleValue)
     {
         $this->visibleValue = $visibleValue;
     }
 
     public function getFilterSql(string $visibilityFieldAlias): string
     {
-        return $visibilityFieldAlias.' = "'.$this->visibleValue.'"';
+        return $visibilityFieldAlias.' = '.$this->getSqlLiteral($this->visibleValue);
+    }
+
+    /**
+     * @param string|int|bool $value
+     */
+    private function getSqlLiteral($value): string
+    {
+        if (is_string($value)) {
+            return '"'.$value.'"';
+        }
+
+        if (is_bool($value)) {
+            return $value ? '1' : '0';
+        }
+
+        return $value;
     }
 }

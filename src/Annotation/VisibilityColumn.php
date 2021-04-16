d2d05f74790f166bf60a6b2f21b8a2849787d4c6
Jano Paetzold <jep@webfactory.de>
2021-04-16 16:12:29 +0200

    Comment VisibilityColumn annotation (Case 125045)

--- src/Annotation/VisibilityColumn.php
+++ src/Annotation/VisibilityColumn.php
@@ -2,7 +2,15 @@
 
 namespace Webfactory\VisibilityFilterBundle\Annotation;
 
+use Webfactory\VisibilityFilterBundle\Filter\Strategy\FilterStrategy;
+
 /**
+ * This annotation marks a field of an entity as the field containing visibility information (e.g. "y" for visible,
+ * "n" for invisible, or a more complicated system). If the VisibilityFilterBundle is set up, it will be interpreted
+ * by an implementation of @see FilterStrategy
+ *
+ * For more information on visibility filtering, consider reading the README.md file of this bundle.
+ *
  * @Annotation
  * @Target("PROPERTY")
  */

2fad7a490c30ed20b50a1b78b907db826df677ca
Matthias Pigulla <mp@webfactory.de>
2022-10-25 17:16:32 +0000

    Korrigiere Konfiguration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,11 +4,15 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         '@Symfony:risky' => true,
-        'array_syntax' => array('syntax' => 'short'),
+        'array_syntax' => ['syntax' => 'short'],
         'no_unreachable_default_argument_value' => false,
-        'braces' => array('allow_single_line_closure' => true),
+        'braces' => ['allow_single_line_closure' => true],
         'heredoc_to_nowdoc' => false,
         'phpdoc_annotation_without_dot' => false,
+        'php_unit_test_annotation' => false,
+        'php_unit_method_casing' => false,
+        'global_namespace_import' => ['import_classes' => true, 'import_constants' => false, 'import_functions' => false],
+        'psr_autoloading' => false,
     ])
     ->setRiskyAllowed(true)
     ->setFinder(
